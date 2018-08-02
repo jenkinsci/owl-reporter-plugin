565e2138409abf5595a9754a0057f1ea34af9f2f
Tarik Demirović <tarik.demirovic@atlantbh.com>
2018-08-02 16:18:00 +0200

    Remove unused import and fix wrong icon file path

--- src/main/java/com/atlantbh/jenkins/owlReporter/OwlReporterBuildAction.java
+++ src/main/java/com/atlantbh/jenkins/owlReporter/OwlReporterBuildAction.java
@@ -1,12 +1,11 @@
 package com.atlantbh.jenkins.owlReporter;
 
-import hudson.model.AbstractBuild;
 import hudson.model.Action;
 import hudson.model.Run;
 
 public class OwlReporterBuildAction implements Action {
     private final static String DISPLAY_NAME = "Owl report";
-    private final static String ICON_FILE_NAME = "/plugin/owlReporter/img/owl.png";
+    private final static String ICON_FILE_NAME = "/plugin/owl-reporter/img/owl.png";
     private final static String TEST_RUN_URI = "/test-runs/%d/test-cases";
 
     private Run<?, ?> build;
